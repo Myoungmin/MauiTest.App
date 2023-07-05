0a4757d22273c7fdbad5a80dd569a82ed6059970
AzureAD\고명민 <miung222@gmail.com>
2023-07-05 22:57:12 +0900

    MainPageViewModel 생성 ICommand 선언

---
+++ ViewModels/MainPageViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace MauiTest.ViewModels
+{
+    public class MainPageViewModel
+    {
+        public ICommand CounterCommand { get; }
+        public ICommand OpenDetailedPageCommand { get; }
+    }
+}
