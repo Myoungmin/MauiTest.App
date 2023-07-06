66c9afb4f07ba4078ec6cb0ea52b2171e9d7bd02
AzureAD\고명민 <miung222@gmail.com>
2023-07-06 22:55:13 +0900

    MVVM에서 NavigationPage 사용
    참고
    https://blog.naver.com/goldrushing/221813759257

--- ViewModels/MainPageViewModel.cs
+++ ViewModels/MainPageViewModel.cs
@@ -1,15 +1,38 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Input;
+﻿using System.Windows.Input;
 
 namespace MauiTest.ViewModels
 {
     public class MainPageViewModel
     {
+        int count = 0;
+
+        public String CounterBtnText { get; set; }  
+
+        public INavigation Navigation { get; set; }
+
         public ICommand CounterCommand { get; }
         public ICommand OpenDetailedPageCommand { get; }
+
+        public MainPageViewModel(INavigation navigation)
+        {
+            Navigation = navigation;
+            CounterCommand = new Command(() => OnCounterClicked());
+            OpenDetailedPageCommand = new Command(async () => await OnDetailsPageClickedAsync());
+        }
+
+        private void OnCounterClicked()
+        {
+            count++;
+
+            if (count == 1)
+                CounterBtnText = $"Clicked {count} time";
+            else
+                CounterBtnText = $"Clicked {count} times";
+        }
+
+        private async Task OnDetailsPageClickedAsync()
+        {
+            await Navigation.PushAsync(new DetailsPage());
+        }
     }
 }
--- Views/MainPage.xaml.cs
+++ Views/MainPage.xaml.cs
@@ -1,29 +1,15 @@
-﻿namespace MauiTest;
+﻿using MauiTest.ViewModels;
+
+namespace MauiTest;
 
 public partial class MainPage : ContentPage
 {
-	int count = 0;
-
+	MainPageViewModel viewModel;
 	public MainPage()
 	{
 		InitializeComponent();
-	}
-
-	private void OnCounterClicked(object sender, EventArgs e)
-	{
-		count++;
-
-		if (count == 1)
-			CounterBtn.Text = $"Clicked {count} time";
-		else
-			CounterBtn.Text = $"Clicked {count} times";
-
-		SemanticScreenReader.Announce(CounterBtn.Text);
-	}
-
-    private async void OnDetailsPageClickedAsync(object sender, EventArgs e)
-    {
-        await Navigation.PushAsync(new DetailsPage());
-    }
+		viewModel = new MainPageViewModel(Navigation);
+		BindingContext= viewModel;
+	}	
 }
 
