6e4960918dbd17bb55c292f38be1bc6825affab3
AzureAD\고명민 <miung222@gmail.com>
2023-07-11 00:01:28 +0900

    BindableProperty를 적용하는 시행착오, 빌드는 성공

--- ViewModels/DetailsPageViewModel.cs
+++ ViewModels/DetailsPageViewModel.cs
@@ -1,23 +1,45 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
 
 namespace MauiTest.ViewModels
 {
-    internal class DetailsPageViewModel
+    internal class DetailsPageViewModel : ObservableObject
     {
+        private float _someScore;
+        public float SomeScore
+        {
+            get => _someScore;
+            set
+            {
+                if (_someScore == value) return;
+                _someScore = value;
+                OnPropertyChanged();
+            }
+        }
+
         public DetailsPageViewModel() 
         {
-            //RectangleDrawable rectangleDrawable = new RectangleDrawable();
+            SomeScore = 100;
         }
     }
 
-    internal class RectangleDrawable : IDrawable
+    internal class RectangleDrawable : BindableObject, IDrawable
     {
+        public static readonly BindableProperty ScoreProperty = BindableProperty.Create(nameof(Score), typeof(float), typeof(RectangleDrawable));
+
+        public float Score
+        {
+            get => (float)GetValue(ScoreProperty);
+            set => SetValue(ScoreProperty, value);
+        }
+
         public void Draw(ICanvas canvas, RectF dirtyRect)
         {
+            var centerPoint = new PointF(320, Score);
+            var circleRadius = 5;
+
+            canvas.FillColor = Colors.Black;
+            canvas.FillCircle(centerPoint, circleRadius);
+
             canvas.StrokeColor = Colors.Red;
             canvas.StrokeSize = 4;
             canvas.DrawRectangle(10, 10, 100, 50);
