af1c703f9bd490bc266cc3c837233c54834789dd
AzureAD\고명민 <miung222@gmail.com>
2023-07-14 00:02:41 +0900

    Chnage Image로 campus.jpg로 이미지 변경

--- ViewModels/ImagePageViewModel.cs
+++ ViewModels/ImagePageViewModel.cs
@@ -6,5 +6,17 @@ namespace MauiTest.ViewModels
     public class ImagePageViewModel : ObservableObject
     {
         public Image Image { get; set; }
+
+        public ICommand ChangeImageCommand { get; }
+
+        public ImagePageViewModel() 
+        {
+            ChangeImageCommand = new Command(OnChangeImageClicked);
+        }
+
+        private void OnChangeImageClicked()
+        {
+            Image.Source = "campus.jpg";
+        }
     }
 }
